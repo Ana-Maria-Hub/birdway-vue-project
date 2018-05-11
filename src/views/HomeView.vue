e4ed881c32d74659f60d6edb300299091ce2d26c
root <pearpaes@gmail.com>
2018-05-11 14:48:15 -0700

    HomeView.vue added

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <HomeComponent />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import HomeComponent from "../components/HomeComponent.vue";
+
+export default defineComponent({
+  name: "HomeView",
+  components: {
+    HomeComponent,
+  },
+});
+</script>
