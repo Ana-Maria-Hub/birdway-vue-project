5cdea890c36f51c20043640814b4efea106a6360
root <pearpaes@gmail.com>
2018-05-24 11:49:54 -0700

    App.vue added

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import HeaderComponent from './components/HeaderComponent.vue';
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+ 
+</script>
+
+
+<template>
+  <body>
+    <div id="app">
+        <HeaderComponent class="header"></HeaderComponent>
+      <router-view></router-view>
+      
+      <div>
+      <FooterComponent></FooterComponent>
+      </div>
+      
+    </div>
+  </body>
+
+</template>
+
+<script>
+  import FooterComponent from './components/FooterComponent.vue';
+  import HeaderComponentVue from './components/HeaderComponent.vue';
+</script>
+
+
+<style>
+
+*{
+  box-sizing: border-box;
+  margin: 0px;
+  padding: 0px;
+  /* font-family: 'Plus Jakarta Sans', sans-serif; */
+}
+
+body{
+  width: 100vw ;
+  overflow-x: hidden;
+}
+.header{
+position:sticky;
+}
+
+
+/* MATERIAL ICONS */
+.material-symbols-outlined {
+  font-variation-settings:
+  'FILL' 0,
+  'wght' 400,
+  'GRAD' 0,
+  'opsz' 48
+}
+</style>
