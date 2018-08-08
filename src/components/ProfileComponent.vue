5de7fa973ec441c7116eb6548fca31b157027fe7
root <pearpaes@gmail.com>
2018-08-08 09:00:02 -0700

    ProfileComponent.vue added

---
+++ src/components/ProfileComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sign-in">
+    <img src="../assets/brand/vista-guia.png" alt="">
+  </div>
+</template>
+
+<style scoped>
+.sign-in {
+  background-color: blueviolet;
+  height: 600px;
+}
+
+img{
+  width: 100vw;
+}
+</style>
+
+<script setup></script>
