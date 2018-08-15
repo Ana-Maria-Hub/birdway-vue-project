a44453fb8ec7809ab86cd014574d6897c48b2d6c
root <pearpaes@gmail.com>
2018-08-15 14:21:35 -0700

    MapasComponent.vue added

---
+++ src/components/MapasComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main-maps">
+    <!-- <GetLocation></GetLocation> -->
+    <div class="map">
+      <h1>Mapa Interactivo Birdway</h1>
+      <MapaCaldas></MapaCaldas>
+      <!-- <MapaColombia></MapaColombia> -->
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import '../assets/style.scss';
+.main-maps{
+  background-color: none;
+  background-image: url('../assets/map/tophograpic.png');
+  background-size: cover;
+}
+.map{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 3em;
+  h1{
+    text-align: center;
+    color:$dark-blue
+  }
+}
+</style>
+
+<script setup>
+import GetLocation from './organism/GetLocation.vue';
+import MapaCaldas from './organism/MapaCaldas.vue';
+import MapaColombia from './organism/MapaColombia.vue';</script>
