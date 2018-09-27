b2aa530b6b5dd2e133a6f62085c649004e257d1d
root <pearpaes@gmail.com>
2018-09-26 18:05:28 -0700

    RutasView.vue added

---
+++ src/views/RutasView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <RutasComponent></RutasComponent>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import RutasComponent from "../components/RutasComponent.vue";
+
+export default defineComponent({
+  name: "RutasView",
+  components: {
+    RutasComponent,
+  },
+});
+</script>
